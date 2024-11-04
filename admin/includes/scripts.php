06bd328c2220fe983f1eea08859cf4dd76f0dae1
Gervis Bermudez <gervisbermudez@outlook.com>
2024-11-04 13:26:55 -0300

    login

---
+++ admin/includes/scripts.php
@@ -0,0 +1,5 @@
+    <!--jQuery-->
+    <script src="/public/vendors/jquery-3.7.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous">
+    </script>
+    <script src="public/js/scripts.js"></script>
--- admin/login.php
+++ admin/login.php
@@ -1,8 +1,10 @@
 <?php
 session_start();
 
+$title = "Wonderland - Admin";
+
 // Simulación de autenticación. Aquí puedes verificar contra una base de datos de usuarios
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
@@ -18,31 +20,82 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 ?>
 
 <!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Iniciar sesión - Panel de Administración</title>
-</head>
-<body>
+<html lang="en">
 
-<h1>Iniciar Sesión</h1>
+<?php include './includes/head.php'?>
 
-<?php
-if (isset($error_message)) {
-    echo "<p style='color:red;'>$error_message</p>";
-}
-?>
-
-<form method="POST" action="">
-    <label for="username">Usuario:</label>
-    <input type="text" name="username" required><br><br>
-
-    <label for="password">Contraseña:</label>
-    <input type="password" name="password" required><br><br>
-
-    <input type="submit" value="Iniciar sesión">
-</form>
-
+<body class="bg-dark">
+    <div id="layoutAuthentication">
+        <div id="layoutAuthentication_content">
+            <main>
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-5">
+                            <div class="card shadow-lg border-0 rounded-lg mt-5">
+                                <div class="card-header">
+                                    <div class="p-2">
+                                        <img src="./public/assets/img/logo_wonderland.png"
+                                            class="img-fluid d-block mx-auto" alt="logo_wonderland">
+                                    </div>
+                                    <h3 class="text-center font-weight-light my-4">Login</h3>
+                                </div>
+                                <?php if (isset($error_message)): ?>
+                                <div class="alert alert-danger alert-dismissible fade show m-2" role="alert">
+                                    <?=$error_message?>
+                                    <button type="button" class="btn-close" data-bs-dismiss="alert"
+                                        aria-label="Close"></button>
+                                </div>
+                                <?php endif?>
+                                <div class="card-body">
+                                    <form method="POST" action="">
+                                        <div class="form-floating mb-3">
+                                            <input class="form-control" id="inputUsername" type="text" name="username"
+                                                placeholder="Usuario" />
+                                            <label for="inputUsername">Usuario</label>
+                                        </div>
+                                        <div class="form-floating mb-3">
+                                            <input class="form-control" id="inputPassword" name="password"
+                                                type="password" placeholder="Password" />
+                                            <label for="inputPassword">Contraseña</label>
+                                        </div>
+                                        <div class="form-check mb-3">
+                                            <input class="form-check-input" id="inputRememberPassword" type="checkbox"
+                                                value="" />
+                                            <label class="form-check-label" for="inputRememberPassword">Remember
+                                                Password</label>
+                                        </div>
+                                        <div class="d-flex align-items-center justify-content-between mt-4 mb-0">
+                                            <a class="small" href="password.html">Forgot Password?</a>
+                                            <button type="submit" name="login" id="login"
+                                                class="btn btn-primary btn-lg btn-block">Login</button>
+                                        </div>
+                                    </form>
+                                </div>
+                                <div class="card-footer text-center py-3">
+                                    <div class="small"><a href="register.html">Need an account? Sign up!</a></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </div>
+        <div id="layoutAuthentication_footer">
+            <footer class="py-4 bg-light mt-auto">
+                <div class="container-fluid px-4">
+                    <div class="d-flex align-items-center justify-content-between small">
+                        <div class="text-muted">Copyright &copy; Your Website 2023</div>
+                        <div>
+                            <a href="#">Privacy Policy</a>
+                            &middot;
+                            <a href="#">Terms &amp; Conditions</a>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    </div>
+    <?php include './includes/scripts.php'?>
 </body>
+
 </html>
