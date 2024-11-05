b9cbb3bcbba6d7f10e3f4c1546562ada65378f19
Gervis Bermudez <gervisbermudez@outlook.com>
2024-11-04 21:21:20 -0300

    actualizo la base de datos

--- admin/sql/tienda_online_v29.sql
+++ admin/sql/tienda_online_v29.sql
@@ -16,8 +16,29 @@
 
 
 -- Volcando estructura de base de datos para tienda_online
+DROP DATABASE IF EXISTS `tienda_online`;
 CREATE DATABASE IF NOT EXISTS `tienda_online` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
 USE `tienda_online`;
+
+-- Volcando estructura para tabla tienda_online.clientes
+DROP TABLE IF EXISTS `clientes`;
+CREATE TABLE IF NOT EXISTS `clientes` (
+  `id_cliente` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) NOT NULL,
+  `apellido` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `telefono` varchar(50) NOT NULL,
+  `direccion` varchar(50) NOT NULL,
+  `fecha_alta` timestamp NULL DEFAULT NULL,
+  `status` int(11) NOT NULL DEFAULT 1,
+  PRIMARY KEY (`id_cliente`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Volcando datos para la tabla tienda_online.clientes: ~2 rows (aproximadamente)
+DELETE FROM `clientes`;
+INSERT INTO `clientes` (`id_cliente`, `nombre`, `apellido`, `email`, `telefono`, `direccion`, `fecha_alta`, `status`) VALUES
+	(5, 'Gervis', 'Bermudez', 'gervis-fakemail@gmail.com', '+541157614613', 'OHiggins 4679 8 87 Nuñez', NULL, 1),
+	(13, 'Karen', 'Bermúdez', 'Karen@gmail.com', '+541157614613', 'OHiggins 4679 8 87 Nuñez', NULL, 1);
 
 -- Volcando estructura para tabla tienda_online.pedido
 DROP TABLE IF EXISTS `pedido`;
@@ -28,9 +49,12 @@ CREATE TABLE IF NOT EXISTS `pedido` (
   `estado` enum('pendiente','completado') DEFAULT 'pendiente',
   `metodo_pago` enum('efectivo') DEFAULT 'efectivo',
   PRIMARY KEY (`id_pedido`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla tienda_online.pedido: ~1 rows (aproximadamente)
+DELETE FROM `pedido`;
+INSERT INTO `pedido` (`id_pedido`, `fecha_pedido`, `monto_total`, `estado`, `metodo_pago`) VALUES
+	(1, '2024-11-04 15:20:27', 1234.00, '', 'efectivo');
 
 -- Volcando estructura para tabla tienda_online.producto
 DROP TABLE IF EXISTS `producto`;
@@ -38,13 +62,18 @@ CREATE TABLE IF NOT EXISTS `producto` (
   `id_producto` int(11) NOT NULL AUTO_INCREMENT,
   `nombre_producto` varchar(100) NOT NULL,
   `descripcion` text DEFAULT NULL,
+  `imagen` text DEFAULT NULL,
   `tipo_torta` enum('entera','mediana','porción') NOT NULL,
   `precio` decimal(10,2) NOT NULL,
   `disponible` tinyint(1) DEFAULT 1,
   PRIMARY KEY (`id_producto`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla tienda_online.producto: ~2 rows (aproximadamente)
+DELETE FROM `producto`;
+INSERT INTO `producto` (`id_producto`, `nombre_producto`, `descripcion`, `imagen`, `tipo_torta`, `precio`, `disponible`) VALUES
+	(9, 'rZGsjWjuIrRdQaj', 'BROVjIyFvZhixVIJIGjZSuYoxdqDVPWisnudumlUhD', '/public/img/uploads/img_672963d6909a59.43585903.jpg', 'entera', 1234.00, 1),
+	(10, 'vrLsoEVXrKOLCtEZvEP', 'fLedSjhoJDvPrQcQYODIpZDqrtTXTn', '/public/img/uploads/img_672964a74e1c26.71295868.jpg', 'entera', 1234.00, 1);
 
 -- Volcando estructura para tabla tienda_online.usuario
 DROP TABLE IF EXISTS `usuario`;
@@ -58,7 +87,8 @@ CREATE TABLE IF NOT EXISTS `usuario` (
   UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla tienda_online.usuario: ~0 rows (aproximadamente)
+DELETE FROM `usuario`;
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
